28102e8884d476206c8dd69f70ce6804fadb1668
ShonTitor <14-10576@usb.ve>
2023-09-05 01:25:11 -0400

    add alternate rarities

--- wixoss.py
+++ wixoss.py
@@ -2,8 +2,27 @@ import requests
 import json
 import csv
 import time
+import sys
+import os
+import io
+from PIL import Image
 
+SAVE_IMAGES = True
 cards = {}
+
+rarity_url = "https://www.takaratomy.co.jp/products/en.wixoss/card/master.php?m=rarity"
+rarities: set = set()
+
+r = requests.get(rarity_url)
+rarity_data = json.loads(r.content)
+rarity_suffix = {
+    item["name"]:item["image_end"]
+    for item in rarity_data["items"]
+}
+
+#rarity_suffix = {'PI': '', 'SR': '', 'L': '', 'L(P)': 'P[EN]', 'R': '', 'R(P)': 'P[EN]', "R(P')": 'PS[EN]', 'C': '', 'C(P)': 'P[EN]', 'ST': '', 'ST(P)': 'P[EN]', 'PR': '', 'PR(P)': 'P[EN]', '???': '', 'DiR': 'D[EN]', 'SCR': 'S[EN]', 'LC': '', 'LC(P)': 'P[EN]', 'LR': 'R[EN]', 'LRP': 'P[EN]', 'SRP': 'P[EN]', 'UR': 'U[EN]', 'TK': 'P[EN]', 'Re': '', 'CO': 'P[EN]'}"""
+
+missing = []
 
 page = 0
 while True:
@@ -14,28 +33,61 @@ while True:
 
     items = json.loads(r.content)["items"]
     if len(items) == 0:
-        print(len(items))
         break
 
     for item in items:
         card_name = item["name"].strip() or " "
         card_no = item["card_no"].strip()  or " "
-        image_url = f"https://www.takaratomy.co.jp/products/en.wixoss/card/thumb/{card_no}.jpg"
+        #image_url = f"https://www.takaratomy.co.jp/products/en.wixoss/card/thumb/{card_no}.jpg"
         card_type = item["card_type"].strip()  or " "
 
         print(card_name, card_no)
+        item_rarities = item["rarity"].split(",")
+        for rarity in item_rarities:
+            suffix = rarity_suffix.get(rarity)
+            code = card_no
+            if suffix and rarity != item_rarities[0]:
+                code = code.replace("[EN]", suffix)
 
-        card_dict = {
-            "name": card_name,
-            "code": card_no,
-            "imageUrl": image_url,
-            "type": card_type
-        }
+            image_url = f"https://www.takaratomy.co.jp/products/en.wixoss/card/thumb/{code}.jpg"
 
-        cards[card_no] = card_dict 
-    time.sleep(0.1)
+            filename = os.path.join("images", f"{code}.jpg")
+            if SAVE_IMAGES and not os.path.exists(filename):
+                print(f"Saving {image_url}")
+                r = requests.get(image_url)
+                time.sleep(0.15)
+                if r.status_code != 200:
+                    print(f"Error: status code {r.status_code}")
+                    missing.append(f"{card_name} ({code})")
+                    continue
+                image_data = io.BytesIO(r.content)
+                img = Image.open(image_data)
+                img.save(filename, optimize=True)
+            elif not os.path.exists(filename):
+                print(f"Checking {image_url}")
+                r = requests.get(image_url)
+                time.sleep(0.15)
+                if r.status_code != 200:
+                    print(f"Error: status code {r.status_code}")
+                    missing.append(f"{card_name} ({code})")
+                    continue
+                
+
+            card_dict = {
+                "name": card_name,
+                "code": code,
+                "imageUrl": image_url,
+                "type": card_type
+            }
+
+            cards[code] = card_dict 
+
+    time.sleep(0.15)
     page += 1
 print(len(cards))
+
+for m in missing:
+    print(m)
 
 def write_file(filename, separator):
     open(filename, 'a').close()
